928de799b1b2e58c030c971fe4ac911438bfee2c
webxpujan <pujan36@gmail.com>
2022-05-27 01:20:38 +0545

    header

--- pages/account/index.js
+++ pages/account/index.js
@@ -1,3 +1,4 @@
+import { Head } from 'next/head';
 import { userService } from '../../services';
 import { Layout } from '../../src/components/account/Layout';
 import { AddEdit } from '../../src/components/admin/AddEdit'
@@ -7,6 +8,9 @@ const Home = () => {
 
   return(
     <Layout>
+      <Head>
+        <meta httpEquiv="content-security-policy" content="upgrade-insecure-requests" />
+      </Head>
       <h1>Welcome {userService.userValue.user.name }!</h1>
       <AddEdit />
     </Layout>
