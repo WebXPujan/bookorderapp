f19d12dc8123253a183a887e55e4c4a6b5b05693
webxpujan <pujan36@gmail.com>
2022-05-27 01:22:11 +0545

    header

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,11 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Book Order App</title>
+        <meta httpEquiv="content-security-policy" content="upgrade-insecure-requests" />
         <meta name="description" content="Online Book order form" />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <meta charSet="UTF-8" />
+        
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
